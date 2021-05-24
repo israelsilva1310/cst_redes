e41695776f8b5c63ccab29cb17731ecbccf79b44
Israel Silva <israelsilva1310@gmail.com>
2021-05-24 20:05:39 -0300

    pronto

---
+++ .bash_history
@@ -0,0 +1,177 @@
+ip a
+shutdown 00
+shutdown -t 00
+sudo shutdown -t 00
+exit
+ip a
+apt update
+sudo apt update
+apt update
+sudo apt install rcconf
+su
+cd 
+ls
+mkdir cst_redes
+ls
+cd cst_redes
+mkdir
+mkdir scripts
+ls
+cd scripts
+ls
+nano assassim_config.sh
+netstats
+su
+ls
+chmod +x assissm_config.sh
+chmod +x assassim_config.sh
+ls
+rcconfig
+rcconf
+su rcconf
+su
+cd 
+ls
+git@github.com:israelsilva1310/cst_redes.git
+git remote add origin git@github.com:israelsilva1310/cst_redes.git
+git branch -M main
+git push -u origin main
+su
+ssh-keygen
+ssh-copy-id username@remote_host
+ssh-copy-id israel@localhost
+ssh-copy-id israelsilva1310@gmail.com
+ls
+cd ./ssh
+cd .ssh
+ls
+cat id_rsa.pub
+cd 
+git push -u origin main
+git status
+ls
+cd cst_redes
+ls
+git status
+git add *.*
+git add *
+git add
+git push
+git push -u origin main
+git
+git status
+git add
+cd scripts
+ls
+git add .
+su
+git add .
+git push
+git push -u origin main
+git status
+git add .
+git status
+git commit 
+git commit -m "submit"
+git config 
+git config  --list
+git status -s
+git add . && git commit -m
+git add  && git commit -m
+git add .
+git commit -m
+git status
+git push 
+git add -A
+git commit -m
+git commit
+git commit -m "mensagem explicando a mudança no código"
+git push -u origin main
+cd ..
+ls
+git add .
+git init
+git add .
+git push
+git push oringin main
+ls
+cd cst_redes
+ls
+git status
+cd ..
+ls
+rm -rf cst_redes
+rm -rf README.md
+ls
+git status
+git push
+git push -u origin main
+ls
+git status
+cd .
+ls
+cd .git
+ls
+cd ..
+cd 
+rm -rf .git
+ls
+ls -la
+git init
+ls -la
+echo "# cst_redes" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git config --global user.name "Israel Silva"
+git config --global user.email "israelsilva1310@gmail.com"
+git commit -m "first commit"
+git branch -M dev
+git remote add origin git@github.com:israelsilva1310/cst_redes.git
+git push -u origin main
+git status
+git add -A
+git status
+git push -u origin main
+git push
+git push -u origin dev
+ls
+mkdir prog_scripts
+cd prog_scripts
+ls
+nano spamassassin.sh
+ls
+git add .
+git push -u origin dev
+Everything up-to-date
+git reset
+git add .
+git commit -m "base"
+git push origin dev
+git stus
+git status
+nano spamassassin.sh
+git add .
+git status
+git update
+git push origin dev
+git reset
+git add .
+git commit -m "base"
+git push origin dev
+cd..
+ls
+cd ..
+ls
+mkdir redes_longa_distancia
+mkdir redes_locais
+mkdir programacao_ii
+ls
+mkdir 
+mkdir programacao_scripts
+ls
+rm -rf programacao_scripts
+ls
+mkdir banco_de_dados
+ls
+ip a
--- prog_scripts/spamassassin.sh
+++ prog_scripts/spamassassin.sh
@@ -1,44 +1,98 @@
-#!/bin/sh
+#!/bin/bash
 
 # Configuracoes iniciais
 #Questões – Questão 2
+
 #– Numa determinada empresa foi implantada uma solução de código livre para
 #tratamento de mensagens e-mails de tipo “spam”. A solução anti-spam adotada foi
 #o Spamassassin, implantado em um servidor Linux.
+
 #● Espera-se que o administrador da rede da empresa saiba quantos spams são
 #detectados diariamente pelo controle anti-spam.
+
 #● Além disso, quando houver mais de um determinado número de spams
 #detectados em um mesmo dia, o administrador da rede da empresa deve ser
 #notificado com um aviso urgente.
+
 #● Como podemos solucionar esse problema? [continua no próximo slide...]
+
 #– Restrições
 #● O número limite de spams deverá ser passado pelo terminal, como parâmetro.
+
 #● O dia a ser analisado também deverá ser passado pelo terminal.
 
+#Informações adicionais
 
-echo "Instalando o SpamAssassin!...Aguarde..."
+#● O Spamassassin é um programa utilizado para controle de spams. Para cada dia do ano que esse programa esteja em 
+#atividade, é criado automaticamente um diretório de quarentena, onde as mensagens bloqueadas por serem suspeitas são armazenadas.
 
-sudo apt-get update
+#– Exemplos de nomes de diretórios: 20061009, 20080621, 20091126 (ano +mês + dia).
+#– Dentro de cada um desses diretórios, há outro diretório chamado “spam”.
 
-apt-get install spamassassin spamc
+#● Dentro desse diretório, há um arquivo para cada spam encontrado.
 
-echo "Avançando..."
+#– Dica
+#● Contar quantos spams foram encontrados em um dia é fácil: Basta ir no diretório referente ao dia, acessar o diretório 
+#“spam” e contar o número de arquivos presentes no diretório.
 
-echo "Criando Grupo"
-groupadd spamd
+
+
+#######    Codigo ##############                    
+echo "Digite a data desejada para verificação: "
+read dtconsulta
+sleep 0.5
+echo "qual quantidade aceitavel de spam"
+read maxspam
+
+if [ -z $dtconsulta ]
+then
+    echo "Você não digitou uma data!"
+else
+    echo "pesquisando, $dtconsulta"
+	sleep 0.850
+fi
+echo "Configurando o filtro!...Aguarde..."
+sleep 0.15
+echo "Avançando...checando pastas"
+
+if [ -e "/home/$USER/prog_scripts/$dtconsulta" ]
+then
+
+echo " O diretorio $dtconsulta já existe, Atualizando aquivo do dia..."
+ls -la /home/$USER/prog_scripts/$dtconsulta/spam/
+
+touch /home/$USER/prog_scripts/$dtconsulta/spam/spam{1..$maxspam}.txt
+
+sleep 0.25
+
+echo "A quantidade atual de spam é: "ls -la|grep -e "^-"|wc -l /home/$USER/prog_scripts/$dtconsulta/spam/
+else
+
+echo " O diretorio $dtconsulta não existe... criando agora!"
+mkdir -p /home/$USER/prog_scripts/$dtconsulta
+
+mkdir -p /home/$USER/prog_scripts/$dtconsulta/spam
+
+touch /home/$USER/prog_scripts/$dtconsulta/spam/spam{1..$maxspam}.txt
+
+ls -l /home/$USER/prog_scripts/$dtconsulta/spam/
+fi
+exit
+
+while true; do
+    echo -e "\e[?5h\e[33;5;1m A L E R T A $(date) ls -la|grep -e "^-"|wc -l"
+    sleep 0.1
+    printf \\e[?5l
+    read -s -n1 -t1 && printf \\e[?5l && break
+done
 
 echo "Avançando...."
+sleep 0.15
 
-echo "Adicionando Usuario..."
-useradd -g spamd -s /bin/false -d /var/log/spamassassin spamd
+echo "Total de spam na data $dtconsulta : "
 
-echo "Criando diretorios..."
-mkdir /var/log/spamassassin
-
-echo "modificando as Permissoes..."
-chown spamd:spamd /var/log/spamassassin
-
-echo "Configurando o SpamAssassin...."
-
-nano /etc/default/spamassassin
+sleep 0.10
+ls -l /home/$USER/prog_scripts/$dtconsulta/spam/
+sleep 0.25
+echo A quantidade atual de spam é: ls -la|grep -e "^-"|wc -l /home/$USER/prog_scripts/$dtconsulta/spam/
 
